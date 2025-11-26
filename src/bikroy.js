284526c7f456bd481f91d322c1f5bf3aa8cf3968
sadiyabhbb <sadiyabhbb@proton.me>
2025-11-26 10:20:55 +0600

    Update bikroy.js

--- src/bikroy.js
+++ src/bikroy.js
@@ -4,7 +4,6 @@ const config = {
     name: "bikroy",
     version: "1.0.0",
     credits: "LIKHON AHMED",
-    permissions: [0],
     description: "Send OTP via Bikroy API",
     usages: "phone",
     cooldown: 0

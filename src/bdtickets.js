7ebce84db6c65b1f6752b804602d92bd7f4ca6d2
sadiyabhbb <sadiyabhbb@proton.me>
2025-11-26 10:21:13 +0600

    Update bdtickets.js

--- src/bdtickets.js
+++ src/bdtickets.js
@@ -4,7 +4,6 @@ const config = {
   name: "bdtickets",
   version: "1.0.0",
   credits: "LIKHON AHMED",
-  permissions: [0],
   description: "Send OTP via BDTickets API",
   usage: "bdtickets <phone>",
   cooldowns: 0,
